2036f3898e51c4072f7a6da369642df3f09a512f
kwing25 <kendracwing@icloud.com>
2022-04-07 22:34:47 -0600

    📝 Examples of Various Web APIs
    Stack Exchange API Vue App, Wikipedia API App, Project Tracker Examples

---
+++ examples/Project Tracker Demo/assets/js/script.js
@@ -0,0 +1,132 @@
+// save reference to important DOM elements
+var timeDisplayEl = $('#time-display');
+var projectDisplayEl = $('#project-display');
+var projectModalEl = $('#project-modal');
+var projectFormEl = $('#project-form');
+var projectNameInputEl = $('#project-name-input');
+var projectTypeInputEl = $('#project-type-input');
+var hourlyRateInputEl = $('#hourly-rate-input');
+var dueDateInputEl = $('#due-date-input');
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+// handle printing project data to the page
+function printProjectData(name, type, hourlyRate, dueDate) {
+  var projectRowEl = $('<tr>');
+
+  var projectNameTdEl = $('<td>').addClass('p-2').text(name);
+
+  var projectTypeTdEl = $('<td>').addClass('p-2').text(type);
+
+  var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
+
+  var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
+
+  var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
+  var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
+
+  var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
+
+  // You can also chain methods onto new lines to keep code clean
+  var totalTdEl = $('<td>')
+    .addClass('p-2')
+    .text('$' + totalEarnings);
+
+  var deleteProjectBtn = $('<td>')
+    .addClass('p-2 delete-project-btn text-center')
+    .text('X');
+
+  // By listing each `<td>` variable as an argument, each one will be appended in that order
+  projectRowEl.append(
+    projectNameTdEl,
+    projectTypeTdEl,
+    rateTdEl,
+    dueDateTdEl,
+    daysLeftTdEl,
+    totalTdEl,
+    deleteProjectBtn
+  );
+
+  projectDisplayEl.append(projectRowEl);
+
+  projectModalEl.modal('hide');
+}
+
+function calculateTotalEarnings(rate, days) {
+  var dailyTotal = rate * 8;
+  var total = dailyTotal * days;
+  return total;
+}
+
+function handleDeleteProject(event) {
+  console.log(event.target);
+  var btnClicked = $(event.target);
+  btnClicked.parent('tr').remove();
+}
+
+// handle project form submission
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
+
+  var projectName = projectNameInputEl.val().trim();
+  var projectType = projectTypeInputEl.val().trim();
+  var hourlyRate = hourlyRateInputEl.val().trim();
+  var dueDate = dueDateInputEl.val().trim();
+
+  printProjectData(projectName, projectType, hourlyRate, dueDate);
+
+  projectFormEl[0].reset();
+}
+
+projectFormEl.on('submit', handleProjectFormSubmit);
+projectDisplayEl.on('click', '.delete-project-btn', handleDeleteProject);
+dueDateInputEl.datepicker({
+  minDate: 1
+});
+
+setInterval(displayTime, 1000);
+
+
+// JS Local Storage Demo-
+// src: https://codepen.io/kwing25/pen/zYdBjba
+
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
+const input = document.getElementById("item");
+let itemsArray = localStorage.getItem("items") ?
+  JSON.parse(localStorage.getItem("items")) : [];
+
+localStorage.setItem("items", JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem("items"));
+
+const liMaker = (text) => {
+  const li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  itemsArray.push(input.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = "";
+});
+
+data.forEach((item) => {
+  liMaker(item);
+});
+
+button.addEventListener("click", function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  itemsArray = [];
+});
